caffa95d0664073daa2374b92f5a5d97da4f208f
KKamtte <swye199@gmail.com>
2021-05-02 11:54:22 +0900

    5/2 algorithm

---
+++ BaekJoon/Lv_1/A+B_step5.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (line) => {
+    const input = line.split(' ');
+
+    console.log(Number(input[0]) + Number(input[1]));
+
+    rl.close();
+}).on('close', () => {
+    process.exit();
+});
