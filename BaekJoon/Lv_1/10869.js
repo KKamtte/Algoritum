073790086f4064a7c7f53a9af356a66fd4ea55bd
KKamtte <swye199@gmail.com>
2021-05-02 12:09:08 +0900

    lv9: Four rule calculations

---
+++ BaekJoon/Lv_1/10869.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (line) => {
+    const input = line.split(' ');
+
+    console.log(Number(input[0]) + Number(input[1]));
+    console.log(Number(input[0]) - Number(input[1]));
+    console.log(Number(input[0]) * Number(input[1]));
+    console.log(Math.floor(Number(input[0]) / Number(input[1])));
+    console.log(Number(input[0]) % Number(input[1]));
+
+    rl.close();
+}).on('close', () => {
+    process.exit();
+});
