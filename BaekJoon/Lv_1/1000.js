98bbe96e6e07535aeeea43cbd219dce1b5c46f30
KKamtte <swye199@gmail.com>
2021-05-04 22:43:00 +0900

    refactor

--- BaekJoon/Lv_1/1000.js
+++ BaekJoon/Lv_1/1000.js
@@ -1,16 +1,16 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.on('line', (line) => {
-    const input = line.split(' ');
+rl.on('line', line => {
+  const input = line.split(' ');
 
-    console.log(Number(input[0]) + Number(input[1]));
+  console.log(Number(input[0]) + Number(input[1]));
 
-    rl.close();
+  rl.close();
 }).on('close', () => {
-    process.exit();
+  process.exit();
 });
--- BaekJoon/Lv_1/10869.js
+++ BaekJoon/Lv_1/10869.js
@@ -1,20 +1,20 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.on('line', (line) => {
-    const input = line.split(' ');
+rl.on('line', line => {
+  const input = line.split(' ');
 
-    console.log(Number(input[0]) + Number(input[1]));
-    console.log(Number(input[0]) - Number(input[1]));
-    console.log(Number(input[0]) * Number(input[1]));
-    console.log(Math.floor(Number(input[0]) / Number(input[1])));
-    console.log(Number(input[0]) % Number(input[1]));
+  console.log(Number(input[0]) + Number(input[1]));
+  console.log(Number(input[0]) - Number(input[1]));
+  console.log(Number(input[0]) * Number(input[1]));
+  console.log(Math.floor(Number(input[0]) / Number(input[1])));
+  console.log(Number(input[0]) % Number(input[1]));
 
-    rl.close();
+  rl.close();
 }).on('close', () => {
-    process.exit();
+  process.exit();
 });
