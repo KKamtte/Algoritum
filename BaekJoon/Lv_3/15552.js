76b15ef778ccc8aedd04cbffdba5afd099952488
KKamtte <swye199@gmail.com>
2021-05-12 22:52:51 +0900

    lv4: fast A+B

---
+++ BaekJoon/Lv_3/15552.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+let X,
+  result = '';
+
+rl.on('line', (line) => {
+  input.push(line);
+  X = +input[0];
+}).on('close', () => {
+  for (let i = 1; i <= X; i++) {
+    const x = +input[i].split(' ')[0];
+    const y = +input[i].split(' ')[1];
+    console.log(x + y);
+  }
+  process.exit();
+});
