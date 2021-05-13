a8850add39709bb0b14512ade2b75a4d0161277c
KKamtte <swye199@gmail.com>
2021-05-13 21:40:15 +0900

    lv1: A+B-5

---
+++ BaekJoon/Lv_4/10952.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on('line', (line) => {
+  input.push(line);
+}).on('close', () => {
+  let i = 0;
+  while (i < input.length - 1) {
+    const x = +input[i].split(' ')[0];
+    const y = +input[i].split(' ')[1];
+    console.log(x + y);
+    i++;
+  }
+  process.exit();
+});
