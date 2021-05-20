582c564502c5951c2117c8dc3caacea497e085a1
KKamtte <swye199@gmail.com>
2021-05-20 22:13:04 +0900

    lv2: sum of number

---
+++ BaekJoon/Lv_7/11720.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on('line', (line) => {
+  input.push(line);
+  if (input.length > 1) {
+    rl.close();
+  }
+}).on('close', () => {
+  const count = +input[0];
+  const number = input[1].split('');
+  let result = 0;
+
+  for (let i = 0; i < count; i++) {
+    result += +number[i];
+  }
+
+  console.log(result);
+
+  process.exit();
+});
